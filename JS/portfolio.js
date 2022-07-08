705012a971d1435f2eac56314b378e26bd399e0d
Cindy Lara <74997705+cindy123jl@users.noreply.github.com>
2022-07-07 19:13:09 -0700

    changes

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -4,8 +4,10 @@ icon.onclick = function(){
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")){
         icon.src = "./img/sunmode.png";
+        typing.src ="./img/video/darkvideo.mp4";
     }
     else{
         icon.src = "./img/moonmode.png";
+        typing.src ="./img/video/pinkvideo.mp4";
     }
 }
